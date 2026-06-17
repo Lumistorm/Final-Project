4eceeee8b71cb56f4f5982698d06bc3067d22627
Lumistorm <118947395+Lumistorm@users.noreply.github.com>
2026-06-16 21:26:11 -0400

    feat: add turning and remove walk loop

--- main.ts
+++ main.ts
@@ -11,9 +11,6 @@ const SERVO_2 = robotbit.Servos.S8
 const SERVO_1_ZERO = 0
 const SERVO_2_ZERO = 180
 
-// initialize variables
-let degrees: number = 0
-
 // initialize arms
 robotbit.Servo(SERVO_1, SERVO_1_ZERO)
 robotbit.Servo(SERVO_2, SERVO_2_ZERO)
@@ -21,33 +18,50 @@ robotbit.Servo(SERVO_2, SERVO_2_ZERO)
 // initialize display
 basic.showIcon(IconNames.Happy)
 
-input.buttonIsPressed(Button.AB):
-    while (true) {
-        // set arms to 60 degrees upwards
-        degrees = 60
-        robotbit.Servo(SERVO_1, SERVO_1_ZERO + degrees)
-        robotbit.Servo(SERVO_2, SERVO_2_ZERO - degrees)
+input.onButtonPressed(Button.AB, function () {
+    // set arms to 60 degrees upwards
+    robotbit.Servo(SERVO_1, SERVO_1_ZERO + 60)
+    robotbit.Servo(SERVO_2, SERVO_2_ZERO - 60)
 
-        // wait 500 ms
-        basic.pause(500)
+    // wait 300 ms
+    basic.pause(300)
 
-        // set arms to 0 degrees
-        degrees = 0
-        robotbit.Servo(SERVO_1, SERVO_1_ZERO + degrees)
-        robotbit.Servo(SERVO_2, SERVO_2_ZERO - degrees)
-
-        // wait 500 ms
-        basic.pause(500)
-    }
+    robotbit.Servo(SERVO_1, SERVO_1_ZERO)
+    robotbit.Servo(SERVO_2, SERVO_2_ZERO)
+})
 
 input.onButtonPressed(Button.A, function () {
-    degrees = 60
-    robotbit.Servo(SERVO_1, SERVO_1_ZERO + degrees)
-    robotbit.Servo(SERVO_2, SERVO_2_ZERO - degrees)
+    // set arms to 60 degrees upwards
+    robotbit.Servo(SERVO_1, SERVO_1_ZERO + 60)
+    robotbit.Servo(SERVO_2, SERVO_2_ZERO - 60)
+
+    // wait 300 ms
+    basic.pause(300)
+
+    robotbit.Servo(SERVO_1, SERVO_1_ZERO + 15)
+    robotbit.Servo(SERVO_2, SERVO_2_ZERO - 80)
+
+    // wait 300 ms
+    basic.pause(300)
+
+    robotbit.Servo(SERVO_1, SERVO_1_ZERO)
+    robotbit.Servo(SERVO_2, SERVO_2_ZERO)
 })
 
 input.onButtonPressed(Button.B, function () {
-    degrees = 0
-    robotbit.Servo(SERVO_1, SERVO_1_ZERO + degrees)
-    robotbit.Servo(SERVO_2, SERVO_2_ZERO - degrees)
+    // set arms to 60 degrees upwards
+    robotbit.Servo(SERVO_1, SERVO_1_ZERO + 60)
+    robotbit.Servo(SERVO_2, SERVO_2_ZERO - 60)
+
+    // wait 300 ms
+    basic.pause(300)
+
+    robotbit.Servo(SERVO_1, SERVO_1_ZERO + 80)
+    robotbit.Servo(SERVO_2, SERVO_2_ZERO - 15)
+
+    // wait 300 ms
+    basic.pause(300)
+
+    robotbit.Servo(SERVO_1, SERVO_1_ZERO)
+    robotbit.Servo(SERVO_2, SERVO_2_ZERO)
 })
